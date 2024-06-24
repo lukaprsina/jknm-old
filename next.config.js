5579096e101021a25fd60b97bdaac70fe4e8aecd
Luka Pršina <prsinaluka@gmail.com>
2024-06-24 21:50:54 +0200

    new pc

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
+import MillionLint from '@million/lint';
 import "./src/env.js";
 import nextMDX from "@next/mdx";
-
 const withMDX = nextMDX({
-  extension: /\.(md|mdx)$/,
+  extension: /\.(md|mdx)$/
 });
 
 /** @type {import('next').NextConfig} */
@@ -10,43 +10,38 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   experimental: {
-    mdxRs: true,
+    mdxRs: true
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: 'https',
-        hostname: 'jamarski-klub-novo-mesto.s3.eu-central-1.amazonaws.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'jamarski-klub-novo-mesto.s3.eu-central-1.amazonaws.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: '*',
-        port: '',
-        pathname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '*',
-        port: '',
-        pathname: '**',
-      },
-    ],
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "picsum.photos",
+      port: "",
+      pathname: "/**"
+    }, {
+      protocol: 'https',
+      hostname: 'jamarski-klub-novo-mesto.s3.eu-central-1.amazonaws.com',
+      port: '',
+      pathname: '/**'
+    }, {
+      protocol: 'http',
+      hostname: 'jamarski-klub-novo-mesto.s3.eu-central-1.amazonaws.com',
+      port: '',
+      pathname: '/**'
+    }, {
+      protocol: 'http',
+      hostname: '*',
+      port: '',
+      pathname: '**'
+    }, {
+      protocol: 'http',
+      hostname: '*',
+      port: '',
+      pathname: '**'
+    }]
     // jamarski-klub-novo-mesto.s3.eu-central-1.amazonaws.com
-  },
+  }
 };
-
-export default withMDX(nextConfig);
+export default MillionLint.next({
+  rsc: true
+})(withMDX(nextConfig));
