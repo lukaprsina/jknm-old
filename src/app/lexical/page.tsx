f46036c9f26f40d4dd7970af123bb69e06fe8cb9
Luka Pršina <prsinaluka@gmail.com>
2024-07-19 11:33:08 +0200

    messing with lexical

--- src/app/lexical/page.tsx
+++ src/app/lexical/page.tsx
@@ -1,21 +1,5 @@
-import Editor from "./editor";
-import Nodes from "./nodes";
-import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import theme from "./theme";
+import LexicalPlayground from "~/lexical/packages/lexical-playground/src/App";
 
-function App(): JSX.Element {
-  return (
-    <LexicalComposer
-      initialConfig={{
-        namespace: "Playground",
-        nodes: Nodes,
-        onError: (error: Error) => {
-          throw error;
-        },
-        theme: theme,
-      }}
-    >
-      <Editor />
-    </LexicalComposer>
-  );
+export default function App() {
+  return <LexicalPlayground />;
 }
--- src/app/lexical/toolbar.tsx
+++
@@ -1,9 +0,0 @@
-import { Dispatch } from "react";
-
-export default function ToolbarPlugin({
-  setIsLinkEditMode,
-}: {
-  setIsLinkEditMode: Dispatch<boolean>;
-}) {
-  return <p>Toolbar</p>;
-}
