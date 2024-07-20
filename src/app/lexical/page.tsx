7a88700c5496d2b5695b88deb3bac93d46a5ee73
Luka Pršina <prsinaluka@gmail.com>
2024-07-20 18:51:41 +0200

    lexical monorepo editor error

---
+++ src/app/lexical/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+// import LexicalApp from "./../../lexical/packages/lexical-playground/src/App";
+import LexicalApp from "@lexical/monorepo/packages/lexical-playground/src/App";
+
+export default function Lexical() {
+  return <LexicalApp />;
+}
