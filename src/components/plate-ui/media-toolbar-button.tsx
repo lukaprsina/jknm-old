efaab1315646d1346e333de9e9111260cbfeb287
Luka Pršina <prsinaluka@gmail.com>
2024-07-21 16:13:23 +0200

    before aws upload

--- src/components/plate-ui/media-toolbar-button.tsx
+++ src/components/plate-ui/media-toolbar-button.tsx
@@ -4,12 +4,33 @@ import { withRef } from "@udecode/cn";
 import {
   type ELEMENT_IMAGE,
   type ELEMENT_MEDIA_EMBED,
-  useMediaToolbarButton,
+  insertMedia,
 } from "@udecode/plate-media";
+import { useEditorRef } from "@udecode/plate-common";
 
 import { Icons } from "./../../components/icons";
 
 import { ToolbarButton } from "./toolbar";
+
+export const useMediaToolbarButton = ({
+  nodeType,
+}: { nodeType?: string } = {}) => {
+  const editor = useEditorRef();
+
+  return {
+    props: {
+      onClick: async () => {
+        await insertMedia(editor, {
+          type: nodeType,
+          getUrl: async () => prompt("Enter the URL: lol") ?? "",
+        });
+      },
+      onMouseDown: (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+      },
+    },
+  };
+};
 
 export const MediaToolbarButton = withRef<
   typeof ToolbarButton,
